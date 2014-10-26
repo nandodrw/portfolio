cdf9844428c084a4e7650752f5e27545368601e3
Fernando Catacora <nando.drw@gmail.com>
2014-10-26 16:02:21 -0500

    finish smoth initializer of smoth scrolling"

--- initializers.js
+++ initializers.js
@@ -2,6 +2,20 @@
 
   $(document).ready(function(){
 
+    var portfolioLocation = 1440;
+    var skillsLocation = 2180;
+    var experienceLocation = 3150;
+    var contactLocation = 4070;
+    var stops = [];
+    stops.push(portfolioLocation);
+    stops.push(skillsLocation);
+    stops.push(experienceLocation);
+    stops.push(contactLocation);
+
+
+    var scrollingStopFlag = true;
+    var previousScroll = window.scrollY;
+
   	var trans_size = $("#box1").width();
   	trans_size = trans_size - 25;
 
@@ -32,6 +46,100 @@
     $('.spects-box-front').css("transform","translate3d(0, 0, "+spectProjectLength+"px)");
     $('.project-spects').css("transform","translate3d(0, 0, -"+spectProjectLength+"px)");
     $('.spects-box-back').css("transform","translate3d(0, 0, -"+spectProjectLength+"px)");
+
+    //*************************************************************
+    // preparing the stick scroll stop for each section of the page
+    //*************************************************************
+
+    var stopIt = function(flag,prev,last,stopPoints){
+      if(flag){
+        for(var i in stopPoints){
+          console.log("other flag:",stopPoints[i] > last > stopPoints[i],"prev:",prev,"last:",last);
+          if(stopPoints[i] === last ||
+            (last > stopPoints[i] && (last - stopPoints[i]) < 30) ||
+            (last < stopPoints[i] && (stopPoints[i]- last) < 30)){
+            return stopPoints[i];
+          }
+        }
+        return false;
+      }else{
+        return false;
+      }
+    };
+
+    //support for webkit
+
+    document.addEventListener('mousewheel', function(e){
+
+        console.log("win position",window.scrollY);
+        previousScroll = window.scrollY;
+        var currentStop = stopIt(scrollingStopFlag,previousScroll,window.scrollY,stops);
+        if (currentStop) {
+          window.scrollTo(0,currentStop);
+          e.preventDefault();
+          setTimeout(function(){scrollingStopFlag = false;},500);
+          // scrollingStopFlag = false;
+        } else {
+          scrollingStopFlag = true;
+        }
+    }, false);
+
+    // support for IE
+
+    // document.attachEvent('onmousewheel', function(e){
+
+    //     console.log("win position",window.scrollY);
+    //     previousScroll = window.scrollY;
+    //     var currentStop = stopIt(scrollingStopFlag,previousScroll,window.scrollY,stops);
+    //     if (currentStop) {
+    //       window.scrollTo(0,currentStop);
+    //       if (!e) var e = window.event;
+    //       e.returnValue = false;
+    //       e.cancelBubble = true;
+    //       setTimeout(function(){scrollingStopFlag = false;},1000);
+    //     } else {
+    //       scrollingStopFlag = true;
+    //     }
+    //      return false;
+    // }, false);
+
+    // support for Opera
+
+    // document.attachEvent('mousewheel', function(e){
+
+    //   console.log("win position",window.scrollY);
+    //   previousScroll = window.scrollY;
+    //   var currentStop = stopIt(scrollingStopFlag,previousScroll,window.scrollY,stops);
+    //   if (currentStop) {
+    //     window.scrollTo(0,currentStop);
+    //     if (!e) var e = window.event;
+    //     e.returnValue = false;
+    //     e.cancelBubble = true;
+    //     return false;
+    //     setTimeout(function(){scrollingStopFlag = false;},1000);
+    //   } else {
+    //     scrollingStopFlag = true;
+    //   }
+    // }, false);
+
+    //support for Mozilla
+
+    document.addEventListener('DOMMouseScroll', function(e){
+
+      console.log("win position",window.scrollY);
+        previousScroll = window.scrollY;
+        var currentStop = stopIt(scrollingStopFlag,previousScroll,window.scrollY,stops);
+        if (currentStop) {
+          window.scrollTo(0,currentStop);
+          // e.stopPropagation();
+          e.preventDefault();
+          // e.cancelBubble = false;
+          return false;
+          setTimeout(function(){scrollingStopFlag = false;},1000);
+        } else {
+          scrollingStopFlag = true;
+        }
+}, false);
 
 	});
 
