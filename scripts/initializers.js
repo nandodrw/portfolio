7a956935b3ca31dcc81cb91e94124ac8ca95b34e
Fernando Catacora <nando.drw@gmail.com>
2014-11-20 21:14:14 -0600

    start adding parallax

--- scripts/initializers.js
+++ scripts/initializers.js
@@ -1,5 +1,17 @@
 (function(){
 
+  // shim layer with setTimeout fallback
+  window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            window.oRequestAnimationFrame      ||
+            window.msRequestAnimationFrame     ||
+            function( callback ){
+              window.setTimeout(callback, 1000 / 60);
+            };
+  })();
+
   $(document).ready(function(){
 
     //ajusting size of section dynamically
